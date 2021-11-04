ef1980f2cef04d3e9a94819f82e36a6b9d54a66c
Georgi <georgy.keranov@gmail.com>
2021-11-04 23:59:08 +0200

    Exclude hard coded array in a jobs_search_terms.txt file

--- main.py
+++ main.py
@@ -3,23 +3,15 @@ from scrapers.linkedin_jobs_on_site import get_on_site_linkedin_jobs_by_search_t
 from scrapers.linkedin_jobs_remote import get_remote_linkedin_jobs_by_search_terms
 from helpers.visualize_data import visualize_data_in_pie_chart
 
-# Here you can change the search terms to the things you want
-# to analyze like for example progamming languages, frameworks, etc.
-jobs_search_terms = [
-	"Python Developer",
-	"JavaScript Developer",
-	"Java Developer",
-	"C# Developer",
-	"C++ Developer",
-	"C Developer",
-	"PHP Developer",
-	"Ruby Developer",
-	"Golang Developer",
-	"Swift Developer",
-];
+# Get all jobs search terms in 'jobs_search_terms.txt' file and convert them into array
+jobs_search_terms = []
+with open('jobs_search_terms.txt') as jobs_search_terms_file:
+	jobs_search_terms = [line.strip() for line in jobs_search_terms_file.readlines()]
+
 
 # indeed_on_site_jobs_count = get_indeed_jobs_by_search_terms(jobs_search_terms)
 # indeed_remote_jobs_count = get_indeed_jobs_by_search_terms(jobs_search_terms, 'remote')
+
 # linkedin_on_site_jobs_count = get_on_site_linkedin_jobs_by_search_terms(jobs_search_terms)
 # linkedin_jobs_remote = get_remote_linkedin_jobs_by_search_terms(jobs_search_terms)
 
