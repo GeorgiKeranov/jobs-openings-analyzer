7f5762be275d3df0b4f6203ff72251c7e891292e
Georgi <georgy.keranov@gmail.com>
2021-11-04 21:12:56 +0200

    Add comments and refactor code in two files for scraping

--- indeed_jobs.py
+++ indeed_jobs.py
@@ -4,46 +4,58 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from visualize_data import visualize_data_in_pie_chart
 
-programming_technologies = [
+# Here you can change the search terms to the things you want
+# to analyze like for example progamming languages, frameworks, etc.
+jobs_search_terms = [
+	"Python Developer",
 	"JavaScript Developer",
 	"Java Developer",
-	"Python Developer",
-	"Swift Developer",
-	"Perl Developer",
-	"Rust Developer",
 	"C# Developer",
-	"SQL Developer",
 	"C++ Developer",
 	"C Developer",
-	"Golang Developer",
 	"PHP Developer",
 	"Ruby Developer",
+	"Golang Developer",
+	"Perl Developer",
+	"Swift Developer",
+	"Rust Developer",
+	"SQL Developer"
 ];
 
-tech_jobs_count = [];
+# Here we will save the jobs count for every search term
+jobs_found_results = [];
 
+# URL for the indeed jobs
 url = "https://www.indeed.com/jobs?";
 
 # Filter only by the remote jobs
 # url = url + 'remotejob=032b3046-06a3-4876-8dfd-474eb5e7ed11&'
 
-for tech in programming_technologies:
-	url_specific = url + 'q=' + urllib.parse.quote(tech);
+# Search by every search term
+for search_term in jobs_search_terms:
+	# Add the search term to the url
+	url_specific = url + 'q=' + urllib.parse.quote(search_term);
 
+	# Create get request with the search term
 	page = urlopen(url_specific)
 	html = page.read().decode("utf-8")
+
+	# Create BeatifulSoup object to get html data easier from the html response
 	soup = BeautifulSoup(html, "html.parser")
 
+	# Search for <div id="searchCountPages"></div> and get the text in it
 	jobs_count_text = soup.find("div", {"id" : "searchCountPages"}).text
+	
+	# We have this text now "Page 1 of 2,358 jobs" and we are converting it to "2,358"
 	jobs_count_text = ((jobs_count_text.split("of "))[1]).split(" jobs")[0]
-	jobs_count = jobs_count_text.replace(",", "")
-	jobs_count = int(jobs_count)
 
-	tech_jobs_count.append(jobs_count);
+	# We have this text now "2,358" so we convert it to "2358" and integer
+	jobs_count = int(jobs_count_text.replace(",", ""))
 
+	jobs_found_results.append(jobs_count);
+
+	# Create the next request after 2 seconds in order to not be detected as a bot
 	time.sleep(2)
 
-print(tech_jobs_count)
-print(programming_technologies)
-
-visualize_data_in_pie_chart(tech_jobs_count, programming_technologies)
+# Visualize data in pie chart
+visualize_data_in_pie_chart(jobs_found_results, jobs_search_terms)
--- linkedin_jobs_worldwide.py
+++ linkedin_jobs_worldwide.py
@@ -4,35 +4,55 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from visualize_data import visualize_data_in_pie_chart
 
-programming_technologies = [
-	"Spring Boot",
-	"Express.js",
-	"Django",
-	"Ruby on Rails",
-	"Laravel",
-];
+# Here you can change the search terms to the things you want
+# to analyze like for example progamming languages, frameworks, etc.
+jobs_search_terms = [
+	"Python Developer",
+	"JavaScript Developer",
+	"Java Developer",
+	"C# Developer",
+	"C++ Developer",
+	"C Developer",
+	"PHP Developer",
+	"Ruby Developer",
+	"Golang Developer",
+	"Perl Developer",
+	"Swift Developer",
+	"Rust Developer",
+	"SQL Developer"
+]
 
-tech_jobs_count = [];
+# Here we will save the jobs count for every search term
+jobs_found_results = []
 
-url = "https://www.linkedin.com/jobs/search?location=Worldwide&geoId=92000000&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0&keywords=";
+# URL for the LinkedIn jobs that are worldwide
+url = "https://www.linkedin.com/jobs/search?location=Worldwide&geoId=92000000&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0&keywords="
 
-for tech in programming_technologies:
-	url_specific = url + urllib.parse.quote(tech);
-
+# Search by every search term
+for search_term in jobs_search_terms:
+	# Add the search term to the url
+	url_specific = url + urllib.parse.quote(search_term)
+	
+	# Create get request with the search term
 	page = urlopen(url_specific)
 	html = page.read().decode("utf-8")
+
+	# Create BeatifulSoup object to get html data easier from the html response
 	soup = BeautifulSoup(html, "html.parser")
 
+	# Search for <label for="f_TRP-3"></label> and get the text in it
 	jobs_count_text = soup.find("label", {"for" : "f_TPR-3"}).text
+
+	# We have this text now "Any Time (143,727)" and we are converting it to "143,727"
 	jobs_count_text = ((jobs_count_text.split("("))[1]).split(")")[0]
-	jobs_count = jobs_count_text.replace(",", "")
-	jobs_count = int(jobs_count)
 
-	tech_jobs_count.append(jobs_count);
+	# We have this text now "143,727" so we convert it to "143727" and integer
+	jobs_count = int(jobs_count_text.replace(",", ""))
 
+	jobs_found_results.append(jobs_count)
+
+	# Create the next request after 2 seconds in order to not be detected as a bot
 	time.sleep(2)
 
-print(tech_jobs_count)
-print(programming_technologies)
-
-visualize_data_in_pie_chart(tech_jobs_count, programming_technologies)
+# Visualize data in pie chart
+visualize_data_in_pie_chart(jobs_found_results, jobs_search_terms)
