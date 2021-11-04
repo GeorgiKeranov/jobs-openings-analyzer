7b0706400bdece1b8b8dd078a64a61c2d48a961c
Georgi <georgy.keranov@gmail.com>
2021-11-04 22:39:21 +0200

    Renamed two of the linkedin_jobs files for more clarity

--- visualize_data.py
+++ visualize_data.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 def visualize_data_in_pie_chart(data_values, data_labels):
-	plt.pie(data_values, labels = data_labels, autopct='%1.1f%%',startangle=90)
+	plt.pie(data_values, labels = data_labels, autopct = '%1.1f%%', startangle = 90)
 	 
 	# plotting legend
 	plt.axis("equal")
