ab02642eb1af09dfb00d8916b038b0f62ed25310
Georgi <georgy.keranov@gmail.com>
2021-11-04 17:08:43 +0200

    Refactor code in linkedin_jobs_worldwide_remote.py

--- linkedin_jobs_worldwide_remote.py
+++ linkedin_jobs_worldwide_remote.py
@@ -1,83 +1,74 @@
 import time
 from scraper import Scraper
 from selenium.webdriver.common.keys import Keys
-from visualize_data import visualize_data
+from visualize_data import visualize_data_in_pie_chart
 
-def scrape_number_of_jobs_by_each_search_term(search_terms):
-	results_for_search_terms = []
-
-	# Go to linkedin and login with username and password or cookies if we have already logged in
-	scraper = Scraper('https://www.linkedin.com')
-	scraper.add_login_functionality('https://www.linkedin.com/login', '#username', '#password', '', 'button[type="submit"]', 'linkedin')
-
-	time.sleep(2)
-	search_input = scraper.find_element('.search-global-typeahead__input')
-	search_input.click()
-
-	time.sleep(2)
-	search_input.send_keys(search_terms[0] + Keys.ENTER)
-
-	time.sleep(5)
-	jobs_button = scraper.find_element('button[aria-label="Jobs"]')
-	jobs_button.click()
-
-	time.sleep(1)
-	location_input = scraper.find_element('input[aria-label="City, state, or zip code"]')
-	location_input.click()
-
-	time.sleep(2)
-	location_input.clear()
-	location_input.send_keys('Worldwide' + Keys.ENTER)
-
-	time.sleep(2)
-	onsite_remote_button = scraper.find_element('button[aria-label="On-site/Remote filter. Clicking this button displays all On-site/Remote filter options."]')
-	onsite_remote_button.click()
-
-	time.sleep(1)
-	remote_jobs_label = scraper.find_element('label[for="workplaceType-2"]')
-	remote_jobs_label.click()
-
-	time.sleep(2)
-	show_results_button = scraper.find_element('.artdeco-hoverable-content--visible button[data-control-name="filter_show_results"]')
-	show_results_button.click()
-
-	time.sleep(3)
-	results = scraper.find_element('.jobs-search-results-list__title-heading small');
-	results_number_text = (results.text).split(' ')[0]
-	results_number = int(results_number_text.replace(',', ''))
-	results_for_search_terms.append(results_number)
-
-	for index, search_term in enumerate(search_terms):
-		if index == 0:
-			continue;
-
-		time.sleep(2)
-		search_input = scraper.find_elements('.jobs-search-box__keyboard-text-input')[0]
-		search_input.click()
-		time.sleep(2)
-		search_input.clear()
-		search_input.send_keys(search_term + Keys.ENTER)
-
-		time.sleep(3)
-		results = scraper.find_element('.jobs-search-results-list__title-heading small');
-		results_number_text = (results.text).split(' ')[0]
-		results_number = int(results_number_text.replace(',', ''))
-		results_for_search_terms.append(results_number)
-  
-	return results_for_search_terms;
-
-search_terms = [
+# Here you can change the search terms to the things you want
+# to analyze like for example progamming languages, frameworks, etc.
+jobs_search_terms = [
 	'Spring Boot',
 	'Laravel',
 	'Ruby on Rails',
 	'Django',
 	'Express.js',
-	'Node.js'
 ]
 
-results_for_search_terms = scrape_number_of_jobs_by_each_search_term(search_terms)
+jobs_found_results = []
 
-print(search_terms)
-print(results_for_search_terms)
+# Initialize the scraper for linkedin
+scraper = Scraper('https://www.linkedin.com')
+# Go to LinkedIn and login with username and password or cookies if we have already logged in
+scraper.add_login_functionality('https://www.linkedin.com/login', '#username', '#password', '', 'button[type="submit"]', 'linkedin')
 
-visualize_data_in_pie_chart(results_for_search_terms, search_terms)
+# Enter the first search term in the search bar
+search_input_selector = '.search-global-typeahead__input'
+scraper.element_send_keys(search_input_selector, jobs_search_terms[0] + Keys.ENTER)
+
+# Filter search results by jobs
+jobs_button_selector = 'button[aria-label="Jobs"]'
+scraper.element_click(jobs_button_selector)
+
+# Filter search results location to be WorldWide
+location_input_selector = 'input[aria-label="City, state, or zip code"]'
+scraper.element_clear(location_input_selector)
+scraper.element_send_keys(location_input_selector, 'Worldwide' + Keys.ENTER)
+
+# Filter the jobs to only Remote ones
+onsite_remote_button_selector = 'button[aria-label="On-site/Remote filter. Clicking this button displays all On-site/Remote filter options."]'
+remote_jobs_label_selector = 'label[for="workplaceType-2"]'
+show_results_button_selector = '.artdeco-hoverable-content--visible button[data-control-name="filter_show_results"]'
+scraper.element_click(onsite_remote_button_selector)
+scraper.element_click(remote_jobs_label_selector)
+scraper.element_click(show_results_button_selector)
+
+# Wait until the new jobs count is loaded
+time.sleep(5)
+
+# Get number of jobs found
+jobs_found = scraper.find_element('.jobs-search-results-list__title-heading small')
+jobs_found_text = (jobs_found.text).split(' ')[0]
+jobs_found_number = int(jobs_found_text.replace(',', ''))
+
+jobs_found_results.append(jobs_found_number)
+
+search_input_jobs_selector = '.jobs-search-box__input--keyword .relative input:nth-child(2)'
+
+for index, search_term in enumerate(jobs_search_terms):
+	if index == 0:
+		continue;
+
+	scraper.element_clear(search_input_jobs_selector)
+	scraper.element_send_keys(search_input_jobs_selector, search_term + Keys.ENTER)
+
+	# Wait until the new jobs count is loaded
+	time.sleep(5)
+
+	# Get number of jobs found
+	jobs_found_text = (jobs_found.text).split(' ')[0]
+	jobs_found_number = int(jobs_found_text.replace(',', ''))
+	jobs_found_results.append(jobs_found_number)
+
+print(jobs_search_terms)
+print(jobs_found_results)
+
+visualize_data_in_pie_chart(jobs_found_results, jobs_search_terms)
