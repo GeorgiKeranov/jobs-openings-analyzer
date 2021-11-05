a4d23f3e3d71b7c9df483e01d6496cfb6d45d45a
Georgi <georgy.keranov@gmail.com>
2021-11-05 18:55:37 +0200

    Refactor code in main.py

---
+++ helpers/main_helper.py
@@ -0,0 +1,44 @@
+# Get all lines from text file as array
+def get_search_terms_from_file(file_name):
+	with open(file_name) as jobs_search_terms_file:
+		jobs_search_terms = [line.strip() for line in jobs_search_terms_file.readlines()]
+
+	if not jobs_search_terms:
+		print('Please add all of the search terms that you want to search by in "jobs_search_terms.txt" file in the main directory.\n')
+		print('Please note that each term have to be on new line.')
+		exit()
+
+	return jobs_search_terms
+
+# Print question and options in console and give the user to choose option
+def get_console_option(question, options):
+	print('\n' + question + ' You have these options to choose from:')
+
+	for option in options:
+		print('    - ' + option)
+
+	option_selected = input('Select one of the above options: ').lower()
+
+	if option_selected not in options:
+		print('\nERROR: Incorrect option selected, please check the options again')
+		exit()
+
+	return option_selected
+
+# Generates chart tile based on the selected site and the type of jobs
+def generate_chart_title(site_selected, type_of_jobs_selected):
+	chart_title = 'Job Openings'
+
+	if type_of_jobs_selected != 'both':
+		chart_title = type_of_jobs_selected.capitalize() + ' ' + chart_title
+
+	if site_selected != 'all':
+		chart_title += ' in ' + site_selected.capitalize()
+
+	return chart_title
+
+# Generate dynamic function name based on the site selected
+def generate_function_name(site_selected):
+	function_name = 'get_' + site_selected + '_jobs_by_search_terms'
+
+	return function_name
--- main.py
+++ main.py
@@ -1,63 +1,35 @@
-from scrapers.indeed_jobs import get_indeed_jobs_by_search_terms
-from scrapers.linkedin_jobs_on_site import get_on_site_linkedin_jobs_by_search_terms
-from scrapers.linkedin_jobs_remote import get_remote_linkedin_jobs_by_search_terms
+import helpers.main_helper as helper
 from helpers.visualize_data import visualize_data_in_grouped_bar_chart
 from helpers.visualize_data import visualize_data_in_bar_chart
+from scrapers.indeed_jobs import get_indeed_jobs_by_search_terms
+from scrapers.linkedin_jobs import get_linkedin_jobs_by_search_terms
 
 # Get all jobs search terms in 'jobs_search_terms.txt' file and convert them into array
-jobs_search_terms = []
-with open('jobs_search_terms.txt') as jobs_search_terms_file:
-	jobs_search_terms = [line.strip() for line in jobs_search_terms_file.readlines()]
+jobs_search_terms = helper.get_search_terms_from_file('jobs_search_terms.txt')
 
-if not jobs_search_terms:
-	print('Please add all of the search terms that you want to search by in "jobs_search_terms.txt" file in the main directory.\n')
-	print('Please note that each term have to be on new line.')
-	exit()
+type_of_jobs_options = ['on_site', 'remote', 'both']
+type_of_jobs_selected = helper.get_console_option('Which type job jobs you want?', type_of_jobs_options)
 
-site_to_get_data_from = input('Which site you want to get data from, these are the options:\n - linkedin\n - indeed\n\nFetch data from: ')
-allowed_sites = ['linkedin', 'indeed']
+site_options = [ 'linkedin', 'indeed' ]
+site_selected = helper.get_console_option('Which site you want to get data from?', site_options)
 
-if not site_to_get_data_from in allowed_sites:
-	print('\nIncorrect option selected, please check the options again')
-	exit()
+print('\nPlease wait, it may take a while to scrape all the data from the site')
 
-type_of_jobs = input('\nWhich types of jobs you want, these are the options:\n - on-site\n - remote\n - both\n\nTypes of jobs: ')
-allowed_types_of_jobs = ['on-site', 'remote', 'both']
+chart_title = helper.generate_chart_title(site_selected, type_of_jobs_selected)
 
-if not type_of_jobs in allowed_types_of_jobs:
-	print('\nIncorrect option selected, please check the options again')
-	exit()
-else:
-	print('\nPlease wait, it may take a while to scrape all the data from the site')
+# Generate function name based on the site selected
+# Example 1 - site selected is 'indeed' so generate "get_indeed_jobs_by_search_terms"
+# Example 2 - site selected is 'linkedin' so generate "get_linkedin_jobs_by_search_terms"
+function_name = helper.generate_function_name(site_selected)
 
-chart_title = 'Job Openings in ' + site_to_get_data_from.capitalize()
-if type_of_jobs is not 'both':
-	chart_title = type_of_jobs.capitalize() + ' ' + chart_title
+# Get both on-site and remote jobs in one chart
+if type_of_jobs_selected == 'both':
+	# Call function with 'function_name' dynamically
+	on_site_jobs_count = globals()[function_name](jobs_search_terms, 'on_site')
+	remote_jobs_count = globals()[function_name](jobs_search_terms, 'remote')
 
-# TODO fix these ugly if conditions with something more dynamic
-if type_of_jobs == 'on-site':
-	if site_to_get_data_from == 'linkedin':
-		jobs_count = get_on_site_linkedin_jobs_by_search_terms(jobs_search_terms)
-	if site_to_get_data_from == 'indeed':
-		jobs_count = get_indeed_jobs_by_search_terms(jobs_search_terms)
-
-	visualize_data_in_bar_chart(jobs_search_terms, jobs_count, chart_title)
-
-if type_of_jobs == 'remote':
-	if site_to_get_data_from == 'linkedin':
-		jobs_count = get_remote_linkedin_jobs_by_search_terms(jobs_search_terms)
-	if site_to_get_data_from == 'indeed':
-		jobs_count = get_indeed_jobs_by_search_terms(jobs_search_terms, 'remote')
-
-	visualize_data_in_bar_chart(jobs_search_terms, jobs_count, chart_title)
-
-if type_of_jobs == 'both':
-	if site_to_get_data_from == 'linkedin':
-		on_site_jobs_count = get_on_site_linkedin_jobs_by_search_terms(jobs_search_terms)
-		remote_jobs_count = get_remote_linkedin_jobs_by_search_terms(jobs_search_terms)
-
-	if site_to_get_data_from == 'indeed':
-		on_site_jobs_count = get_indeed_jobs_by_search_terms(jobs_search_terms)
-		remote_jobs_count = get_indeed_jobs_by_search_terms(jobs_search_terms, 'remote')
-	
 	visualize_data_in_grouped_bar_chart(jobs_search_terms, on_site_jobs_count, remote_jobs_count, chart_title)
+else:
+	jobs_count = globals()[function_name](jobs_search_terms, type_of_jobs_selected)
+
+	visualize_data_in_bar_chart(jobs_search_terms, jobs_count, chart_title)
