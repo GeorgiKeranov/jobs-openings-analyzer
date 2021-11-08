30c975de730a0dd5b93047f56e8b7a527f2907b4
Georgi <georgy.keranov@gmail.com>
2021-11-08 18:18:38 +0200

    Remove headless option because it can cause detection as a bot

--- helpers/scraper.py
+++ helpers/scraper.py
@@ -36,7 +36,6 @@ class Scraper:
 			'--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
 			'--start-maximized',
 			'--disable-blink-features=AutomationControlled',
-			'--headless'
 		]
 
 		experimental_options = [
